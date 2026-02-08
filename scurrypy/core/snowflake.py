53bfae39bb453ba186e827c0e3ea017370717c44
Furmissile <squirrelmissile.mail@gmail.com>
2026-02-07 19:38:02 -0500

    added docs to snowflake

--- scurrypy/core/snowflake.py
+++ scurrypy/core/snowflake.py
@@ -7,24 +7,30 @@ class Snowflake(int):
 
     @property
     def timestamp_ms(self) -> int:
+        """Timestamp associated with the ID as an integer in milliseconds (ms)."""
         return (self >> 22) + DISCORD_EPOCH
 
     @property
     def datetime(self) -> datetime:
+        """Timestamp associated with the ID as a `datetime` object."""
         return datetime.fromtimestamp(self.timestamp_ms / 1000, tz=timezone.utc)
 
     @property
     def isoformat(self) -> str:
+        """Timestamp associated with the ID as a string in ISO8601 format."""
         return self.datetime.isoformat()
 
     @property
     def worker_id(self) -> int:
+        """Worker ID associated with the ID."""
         return (self & 0x3E0000) >> 17
 
     @property
     def process_id(self) -> int:
+        """Process ID associated with the ID."""
         return (self & 0x1F000) >> 12
 
     @property
     def increment(self) -> int:
+        """Increment associated with the ID."""
         return self & 0xFFF
